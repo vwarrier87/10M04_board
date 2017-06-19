e3720918b7002cd1261acdc7a865e99847b4952b
Varun Warrier <cvarwarrier@gmail.com>
2017-06-20 01:47:49 +0530

    Just added some files

---
+++ tiva/copy_main_file.sh
@@ -0,0 +1,3 @@
+cp /home/varun/iitb/wel/ti/usb_dev_bulk/usb_dev_bulk.c /home/varun/iitb/wel/digital_lab/10M_board/tiva/usb_dev_bulk.c
+cp /home/varun/iitb/wel/ti/usb_dev_bulk/jtag.c /home/varun/iitb/wel/digital_lab/10M_board/tiva/jtag.c
+cp /home/varun/iitb/wel/ti/usb_dev_bulk/jtag.h /home/varun/iitb/wel/digital_lab/10M_board/tiva/jtag.h
